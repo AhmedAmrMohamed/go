a1a79ab8b2ba48f24ef87f62324e8dd70c10c775
theunderd0g <ahmedbonumstelio@gmail.com>
2017-11-30 00:03:22 +0200

    Update go00.cpp

--- go00.cpp
+++ go00.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 char board[11][11];
-char p[11][11]={0};
+char p[11][11]={0}; //the playing board
 int dx[4]={0,0,1,-1},dy[4]={1,-1,0,0};//up,down,right,left
 std::queue<pair <int,int > > dir;
 void display()
-{for(int i=0;i<=9;i++){for(int j=0;j<=9;j++)cout<<p[i][j]<<' ';cout<<endl;}}
+{
+	//prints the playing board.
+	for(int i=0;i<=9;i++){for(int j=0;j<=9;j++)cout<<p[i][j]<<' ';cout<<endl;}
+}
 void loogingmoves(int x,int y,bool move)
 {
 	FILE *logmove;
@@ -35,7 +38,6 @@ bool load()
 	int turn;
 	FILE *save = fopen("save.txt","r");
 	fscanf(save,"%d",&turn);
-	// cout<<save;
 	for(int i=1;i<=9;i++)
 	{
 
@@ -60,21 +62,20 @@ void copy()
 	for(int i=1;i<=9;i++)for(int j=1;j<=9;j++)board[i][j]=p[i][j];
 }
 
-int valid(int a ,int b){return a>=1&&a<=9&&b>=1&&b<=9;}
+int valid(int a ,int b){return a>=1&&a<=9&&b>=1&&b<=9;} //check if a (a,b)belongs to the board
 bool liberity(int a ,int b)
 {
-
+  //return true iff (a,b) has at least 1 liberity
 	for(int i=0;i<4;i++)
 	{
 		if(!valid(a+dx[i],b+dy[i]))continue;
-		//cout<<a+dx[i]<<' '<<b+dy[i]<<board[a+dx[i]][b+dy[i]]<<endl;
 		 if(board[a+dx[i]][b+dy[i]]=='.')return true;
-		// cout<<a<<' '<<b<<endl;
 	}
 	return false;
 }
-bool strliberity(int a,int b) //liberity of string that has a,b
+bool strliberity(int a,int b)
 {
+	//return true iff at least the string has on stone in liberity state.
 	char c= board[a][b];
 	std::queue<pair<int ,int > > q;
 	q.push({a,b});
@@ -115,6 +116,7 @@ void adj(int a,int b,char trplay)//determine which stones belongs to strin that 
 }
  void capture(int a,int b)
  {
+	 //remove the string a,b belongs to.
 	 char c=board[a][b];
 	 queue<pair<int,int> > q;
 	 q.push({a,b});
@@ -132,9 +134,9 @@ void adj(int a,int b,char trplay)//determine which stones belongs to strin that 
 		 }
 	 }
  }
- void bfs(int x,int y,char c )
+ void floadfill(int x,int y,char c )
  {
-
+	  //mark territories
    queue<pair <int,int> >q;
    q.push({x,y});
    while(!q.empty())
@@ -146,11 +148,10 @@ void adj(int a,int b,char trplay)//determine which stones belongs to strin that 
        int a = xx+dx[i],b=yy+dy[i];
        if(!valid(a,b))continue;
        if(board[a][b]=='X'||board[a][b]=='O'||board[a][b]=='D'||board[a][b]==tolower(c)||board[a][b]=='t')continue;
-     //  cout<<a<<' '<<b<<' '<<board[a][b]<<' '<<c<<endl;
        if(board[a][b]=='.')board[a][b]=tolower(c);
        else if(board[a][b]=='x'&&c=='O')board[a][b]='t';
        else if(board[a][b]=='o'&&c=='X'){board[a][b]='t';}
-       else {board[a][b]='D';}//cout<<a<<' '<<b<<endl;}
+       else {board[a][b]='D';}
        q.push({a,b});
      }
    }
@@ -162,7 +163,7 @@ void adj(int a,int b,char trplay)//determine which stones belongs to strin that 
 	 int cnx=0,cny=0;
 	 for(int i=1;i<=9;i++)for(int j=1;j<=9;j++)
 	 {
-		 if(board[i][j]=='X'||board[i][j]=='O'){bfs(i,j,board[i][j]);}
+		 if(board[i][j]=='X'||board[i][j]=='O'){floadfill(i,j,board[i][j]);}
 	 }
 	 for(int i=1;i<=9;i++)
 	 {
@@ -172,22 +173,22 @@ void adj(int a,int b,char trplay)//determine which stones belongs to strin that 
 			 else if(tolower(board[i][j])=='o')++cny;
 		 }
 	 }
-	 cnx>cny?printf("X wins\n"):printf("O wins\n");;
+	 if(cnx==cny)printf("draw\n");
+	 else {cnx>cny?printf("X wins\n"):printf("O wins\n");}
+
 	 printf("X %d O %d\n",cnx,cny);
 	 cout<<"CREDITS\n";
 	 cout<<"Developed by : Ahmed Amr\nTested : Tobros Balil ;)\n";
  }
 int main()
 {
-	 //load();return 0;
-	//freopen("testing.txt","r",stdin);
 	fclose(fopen("moves.txt","w"));//discard all content of moves.txt
 	string corr;
-	int x,spare,y,nx,ny; //true-->x
+	int x,spare,y,nx,ny; //true-->x turns
 	bool turn =true,checkinput=true,pass=false,validpass;
 	char inp;
 	string s="123456789";
-	for(int i=0;i<=9;i++){p[0][i+1]=s[i];p[i+1][0]=s[i];}
+	for(int i=0;i<=9;i++){p[0][i+1]=s[i];p[i+1][0]=s[i];} //numbering the raws and columns
 	for(int i=1;i<=9;i++)for(int j=1;j<=9;j++)p[i][j]='.';
 	while(true)
 	{
@@ -199,18 +200,17 @@ int main()
 			else cout<<"O turn :\n";
 			getline(cin,corr);
 			if(corr=="load"){turn=load();continue;}
-			if(corr=="save"){save(turn);return 0;}
-			if(corr!="pass"){
-			if(corr.size()!=3){cout<<"invalid move...\n";continue;}
-			if(corr[1]!=' '){cout<<"invalid move...\n";continue;}
-			x=corr[0]-'0';
-			y=corr[2]-'0';}
+			if(corr=="ave"){save(turn);return 0;}
+			if(corr!="pass")
+			 {
+					if(corr.size()!=3){cout<<"invalid move...\n";continue;}
+					if(corr[1]!=' '){cout<<"invalid move...\n";continue;}
+					x=corr[0]-'0';
+					y=corr[2]-'0';
+				}
 			else x=-1;
-		// cin>>x;
-		// while(x>9||x<1){cout<<"out of the board...\n"; cin >> x;}
-		if(x==-1&&pass){clcpoints();return 0;}
+    	if(x==-1&&pass){clcpoints();return 0;}
 		else if(x==-1&&!pass){pass=1;turn=!turn;checkinput=false;continue;}
-		//cin>>y;
 		pass=0;
 
 		if(p[x][y]!='.'||!valid(x,y)){cout<<"can\'t play there.\n";continue;}
@@ -220,7 +220,6 @@ int main()
 		if(turn){inp='X';turn=false;}
 		else {inp='O';turn=true;}
 		while(!dir.empty())dir.pop();
-		copy();
     adj(x,y,inp);
 		if(dir.empty())
 				{
